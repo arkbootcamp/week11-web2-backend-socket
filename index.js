56046a585635e3e62dd19144f579b4d58d0da3dc
Alfa <900123@telkom.co.id>
2022-07-01 17:37:24 +0700

    add chat group

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const app = express()
 const http = require('http')
 const httpServer = http.createServer(app)
 const PORT = 4000
+// app.use(cors())
+// app.use('/v1', route)
 const io = new Server(httpServer, {
     cors:{
         origin: 'http://localhost:3000'
@@ -13,14 +15,36 @@ const io = new Server(httpServer, {
 
 io.on('connection', (socket)=>{
     console.log(`ada perankat yg terhubung dengan id ${socket.id}`);
-    socket.on('message', (data)=>{
-        // socket.emit('messageBE', {message: data, date: new Date()})
-        // socket.broadcast.emit('messageBE', {message: data, date: new Date()})
-        // io.emit('messageBE', {message: data, date: new Date()})
-        socket.broadcast.to('0ffppraFNq1bEKHfAAAF').emit('messageBE', {message: data, date: new Date()})
+
+    socket.on('inisialRoom', ({room, username})=>{
+        console.log(room);
+        socket.join(`room:${room}`)
+        // socket.broadcast.t
+        socket.broadcast.to(`room:${room}`).emit('notifAdmin', {
+            sender: 'Admin',
+            message: `${username} bergabung dalam group`,
+            date: new Date().getHours()+':'+ new Date().getMinutes()
+        })
     })
+
+    socket.on('sendMessage', ({room, sender, message})=>{
+        console.log('roomSender', room);
+        io.to(`room:${room}`).emit('newMessage', {
+            sender: sender,
+            message: message,
+            date: new Date().getHours()+':'+ new Date().getMinutes()
+        })
+    })
+
+    // socket.on('message', ({idSocket, message})=>{
+    //     // socket.emit('messageBE', {message: data, date: new Date()})
+    //     // socket.broadcast.emit('messageBE', {message: data, date: new Date()})
+    //     // io.emit('messageBE', {message: data, date: new Date()})
+    //     socket.to(idSocket).emit('messageBE', {message: message, date: new Date()})
+    // })
     socket.on('disconnect', ()=>{
         console.log(`ada perangkat yg terputus dengan id ${socket.id}`);
+        // userModel.deleteUserbyId()
     })
 })
 
